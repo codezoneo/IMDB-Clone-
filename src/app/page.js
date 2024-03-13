91b205fad12c9d968aa733d68795df012e04068c
codezoneo <anuragtri83@gmail.com>
2024-03-13 15:52:06 +0530

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>IMDB App</h1>
+      <h1>Home</h1>
     </main>
   );
 }
